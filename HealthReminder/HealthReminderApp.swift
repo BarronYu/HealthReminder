2a7a1160dc743769776ac868f0a606b7f8bc3e08
yuboyang03 <yuboyang03@kuaishou.com>
2025-08-14 16:14:39 +0800

    Initial Commit

---
+++ HealthReminder/HealthReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthReminderApp.swift
+//  HealthReminder
+//
+//  Created by Barron Yu on 2025/8/14.
+//
+
+import SwiftUI
+
+@main
+struct HealthReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthReminderTests/HealthReminderTests.swift
@@ -0,0 +1,16 @@
+//
+//  HealthReminderTests.swift
+//  HealthReminderTests
+//
+//  Created by Barron Yu on 2025/8/14.
+//
+
+import Testing
+
+struct HealthReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
